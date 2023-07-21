c061d0d68a66ae049ed50692d0c796f3ebe4456b
BKkom <seoy1108@gmail.com>
2023-07-21 19:07:56 +0900

    #4 users array
    유저 map 메서드 사용

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,29 +1,13 @@
 import { useSearchParams } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import { useState } from "react";
-import Trainers from "../Trainers";
-import Manager from "../Manager";
 
 const Login = () => {
-    const [user, setUser] = useState<any>();
-    const [searchParams] = useSearchParams();
-    const type = searchParams.get("type");
+  const [user, setUser] = useState<any>();
+  const [searchParams] = useSearchParams();
+  const type = searchParams.get("type");
 
-    return (
-        <>
-            {user ? (
-                <>
-                    {type === "Trainer" && <Trainers user={user} />}
-                    {type === "Manager" && <Manager user={user} />}
-                </>
-            ) : (
-                <LoginForm
-                    type={type}
-                    setUser={setUser}
-                />
-            )}
-        </>
-    );
+  return <>{user ? <></> : <LoginForm type={type} setUser={setUser} />}</>;
 };
 
 export default Login;
