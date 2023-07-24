ef0525d88c690aef19d33466b8096ae750b96584
ateals <ateals@icloud.com>
2023-07-24 21:54:47 +0900

    axios + react-query

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
 import { UserApi } from "../../api";
 
-const LoginForm = ({ type, setUser }) => {
-    const [values, handleChange, handleSubmit] = useForm({ id: "", password: "" }, async (obj) => {
+const LoginForm = () => {
+    const navigation = useNavigate();
+
+    const useSubmit = async (obj) => {
         const { username: name, password } = obj;
-        const { data, status } = await UserApi.login(name, password);
-        console.log(status, data);
-    });
+        const { data } = await UserApi.login(name, password);
+        if (data.role === "Admin" || data.role === "Trainer") navigation("/");
+        console.log(data);
+    };
+
+    const [, handleChange, handleSubmit] = useForm({ id: "", password: "" }, useSubmit);
+
     return (
         <form onSubmit={handleSubmit}>
-            <h1>{type} 로그인</h1>
+            <h1> 로그인</h1>
             <input
                 type="id"
                 name="username"
@@ -27,11 +33,6 @@ const LoginForm = ({ type, setUser }) => {
                 type="submit"
                 value="로그인"
             />
-            <div>
-                <Link to="/login?type=Manager">Manager Login &rarr;</Link>
-                <br />
-                <Link to="/login?type=Trainer">Trainer Login &rarr;</Link>
-            </div>
         </form>
     );
 };
