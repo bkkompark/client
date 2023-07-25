522c6a34ff8267f918cd8ce2a458c6d9b04a802c
bkkom <seoy1108@gmail.com>
2023-07-25 15:15:01 +0900

    Table 컴포넌트 분리

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <>
-            <Link to="/">Home</Link>&nbsp;
-            <Link to="/about">About</Link>&nbsp;
-            <Link to="/logout">logout</Link>&nbsp;
-        </>
-    );
+  return (
+    <>
+      <Link to="/">Home</Link>&nbsp;
+      <Link to="/about">About</Link>&nbsp;
+      <Link to="/logout">logout</Link>&nbsp;
+      <Link to="/admin">Admin</Link>&nbsp;
+    </>
+  );
 };
 
 export default Nav;
--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,19 +1,28 @@
 import { useState, useEffect } from "react";
 import { TrainerApi } from "../../api";
+import Table from "../../components/Table";
 
-export const TrainerTable = () => {
+export const TrainerAll = () => {
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState<any>([]);
 
   const getUsers = async () => {
-    const { data } = await TrainerApi.getTrainerAll();
-    const { data: data2 } = await TrainerApi.getUnknownAll();
-    setUsers(data.map((i: any) => ({ ...i, role: "Trainer" })));
-    setUsers((users: any[]) => [
-      ...users,
-      ...data2.map((i: any) => ({ ...i, role: "Unknown" })),
-    ]);
-    console.log(users);
+    const { data } = await TrainerApi.getUserAll();
+    console.log(data);
+    setUsers(
+      data.map(
+        ({ email, name, gender, age, phone, role }: any, index: any) => ({
+          index,
+          email,
+          name,
+          gender,
+          age,
+          phone,
+          role,
+        })
+      )
+    );
+    // console.log(users);
     setLoading(false);
   };
 
@@ -24,42 +33,10 @@ export const TrainerTable = () => {
   return <>{!loading && <Table title={"트레이너 목록"} arr={users} />}</>;
 };
 
-export const Table = <T extends {}>({
-  title,
-  arr,
-}: {
-  title: string;
-  arr: T[];
-}) => {
-  return (
-    <>
-      <h1>{title}</h1>
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(arr[0]).map((col, index) => (
-              <th key={index}>{col}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {arr.map((obj, index) => (
-            <tr key={index}>
-              {Object.values(obj).map((value: any, index) => (
-                <td key={index}>{value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
-};
-
 export const Admin = () => {
   return (
     <>
-      <TrainerTable />
+      <TrainerAll />
     </>
   );
 };
