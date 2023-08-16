02db3d463c09403ba719bba2ed9a3701827b8aa1
bkkom <seoy1108@gmail.com>
2023-08-16 15:08:16 +0900

    userForm과 userTable

--- src/api/UserApi.tsx
+++ src/api/UserApi.tsx
@@ -2,18 +2,22 @@ import Axios from "axios";
 import { JoinObject } from "../pages/Join/TrainerJoin";
 
 const axios = Axios.create({
-    baseURL: "/api",
-    timeout: 3000,
-    headers: {
-        "Content-Type": "application/json",
-    },
-    validateStatus: (status) => status < 500,
+  baseURL: "/",
+  timeout: 3000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  validateStatus: (status) => status < 500,
 });
 
 const UserApi = {
-    login: (email: string, password: string) => axios.post("login", { email, password }),
-    logout: () => axios.post("logout"),
-    join: (obj: JoinObject) => axios.post("/join", obj),
+  login: (email: string, password: string) =>
+    axios.post("login", { email, password }),
+  logout: () => axios.post("logout"),
+  join: (obj: JoinObject) => axios.post("/join", obj),
+  enroll: (name: string, age: number, gender: string, phone: string) =>
+    axios.post("customers", { name, age, gender, phone }),
+  getUserAll: () => axios.get("/customers"),
 };
 
 export default UserApi;
--- src/pages/Trainer/index.tsx
+++ src/pages/Trainer/index.tsx
@@ -5,104 +5,43 @@ import { TrainerApi, UserApi } from "../../api";
 import TableHeader from "../../components/TableHeader";
 import UserDetail from "./UseDetail";
 
-const users = [
-  {
-    email: "my-email",
-    name: "test",
-    gender: "male",
-    age: 12,
-    phone: "010-0000-0000",
-    id: "1",
-    role: "User",
-  },
-  {
-    email: "my-email22",
-    name: "test2",
-    gender: "female",
-    age: 34,
-    phone: "010-0000-0120",
-    id: "2",
-    role: "User",
-  },
-  {
-    email: "my-email33",
-    name: "test3",
-    gender: "female",
-    age: 55,
-    phone: "010-0000-0120",
-    id: "3",
-    role: "User",
-  },
-  {
-    email: "my-email44",
-    name: "test4",
-    gender: "male",
-    age: 66,
-    phone: "010-0000-0120",
-    id: "4",
-    role: "User",
-  },
-];
-
-export type enrollObject =
-  | {}
-  | {
-      name: string;
-      gender: "남자" | "여자";
-      age: number;
-      phone: string;
-    };
+export type enrollObject = {
+  name: string;
+  age: number;
+  gender: "남자" | "여자";
+  phone: string;
+};
 
 const UserForm = () => {
-  const fn = async () => {
-    const res = await fetch(`/api/login`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json", // content가 json 파일
-      },
-      body: JSON.stringify({ email: "aa@naver.com", password: "1212" }),
-    });
-    console.log(await res.json());
-
-    // const response = await fetch(`/trainers`, {
-    //   method: "get",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
-    // console.log("fetch ", response);
-    // // console.log(await response.json()); // fetch
-    // const res = await TrainerApi.getTrainerAll(); // axios
-    // console.log("axios ", res);
+  // callback 함수
+  const enrollUser = async (userInfo: enrollObject) => {
+    console.log(userInfo);
+    const { name, age, gender, phone } = userInfo;
+    console.log(name, age, gender, phone);
+    const { data } = await UserApi.enroll(name, age, gender, phone);
+    console.log(data);
   };
-  useEffect(() => {
-    fn();
-  });
 
-  // const [values, handleChange, handleSubmit, error] = useForm<
-  //   enrollObject | {}
-  // >({}, async (a: any) => {
-  //   console.log(a);
-
-  //   const { data } = await UserApi.join(values);
-  //   console.log(data);
-  // });
+  const [values, handleChange, handleSubmit, error] = useForm<enrollObject>(
+    { name: "", age: 0, gender: "여자", phone: "" },
+    enrollUser // callback 함수가 promise를 반환
+  );
 
   return (
     <>
-      {/* <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <h1>회원 등록</h1>
         <input type="" name="name" placeholder="NAME" onChange={handleChange} />
-        <input
-          type=""
-          name="gender"
-          placeholder="GENDER"
-          onChange={handleChange}
-        />
         <input
           type="number"
           name="age"
           placeholder="AGE"
+          onChange={handleChange}
+        />
+        <input
+          type=""
+          name="gender"
+          placeholder="GENDER"
           onChange={handleChange}
         />
         <input
@@ -112,42 +51,50 @@ const UserForm = () => {
           onChange={handleChange}
         />
         <input type="submit" value="회원 등록" />
-      </form> */}
+      </form>
     </>
   );
 };
 
 const UserTable = () => {
-  const [selectedUser, setSelectedUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<any>([]);
 
-  const handleBtnClicked = (userId: any) => {
-    // 버튼을 클릭하면 선택한 사용자의 id를 상태에 저장
-    setSelectedUser(userId);
+  const fetchUsers = async () => {
+    const response = await UserApi.getUserAll();
+    console.log(response);
+    console.log(response.data);
+
+    const users = response.data;
+
+    setUsers(users);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [loading]);
 
   return (
     <>
       <h1>회원 목록</h1>
       <table>
-        <TableHeader columns={["email", "name", "gender", "age", "phone"]} />
+        <TableHeader
+          columns={["id", "name", "age", "gender", "phone", "trainer"]}
+        />
         <tbody>
           {users.map((user: any) => (
             <tr key={user.id}>
-              <td>{user.email}</td>
+              <td>{user.id}</td>
               <td>{user.name}</td>
-              <td>{user.gender}</td>
               <td>{user.age}</td>
+              <td>{user.gender}</td>
               <td>{user.phone}</td>
-              <button onClick={() => handleBtnClicked(user.id)}>
-                상세 정보
-              </button>
+              <td>{user.trainer}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      {selectedUser && (
-        <UserDetail user={users.find((user) => user.id === selectedUser)} />
-      )}
     </>
   );
 };
@@ -155,8 +102,8 @@ const UserTable = () => {
 export const Trainer = () => {
   return (
     <>
-      <UserTable />
       <UserForm />
+      <UserTable />
     </>
   );
 };
