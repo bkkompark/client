a8652d61ad0aa9196d0ea71a46e01d87c3f2cde8
bkkom <seoy1108@gmail.com>
2023-08-31 20:10:13 +0900

    feat: 회원 컴포넌트 생성 및 select event 감지

--- src/pages/Trainer/index.tsx
+++ src/pages/Trainer/index.tsx
@@ -1,12 +1,54 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { TrainerApi } from "../../api";
+import { CustomerApi } from "../../api";
+import TableHeader from "../../components/TableHeader";
+
+const CustomerByTrainer = () => {
+  const [loading, setLoading] = useState(true);
+  const [customers, setCustomers] = useState<any>([]);
+
+  const getCustomers = async () => {
+    const { data: customers } = await CustomerApi.getCustomerAll();
+    // console.log("customer : ", customers);
+    setCustomers(customers);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCustomers();
+  }, [loading]);
+
+  return (
+    <>
+      <table>
+        <TableHeader
+          columns={["id", "name", "age", "gender", "phone", "trainer", ""]}
+        />
+        <tbody>
+          {customers.map((user: any) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+              <td>{user.gender}</td>
+              <td>{user.phone}</td>
+              <td>{user.trainer ? user.trainer.name : ""}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 const Trainer = () => {
   const [loading, setLoading] = useState(true);
   const [trainers, setTrainers] = useState<any>([]);
+  const [selectedId, setSelectedTrainerId] = useState<number | null>(null);
 
   const getTrainers = async () => {
     const { data: trainers } = await TrainerApi.getTrainerAll();
+    // console.log("trainers : ", trainers);
     setTrainers(trainers);
     setLoading(false);
   };
@@ -15,18 +57,26 @@ const Trainer = () => {
     getTrainers();
   }, [loading]);
 
+  const handleTrainer = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
+    const selectedId = parseInt(e.target.value);
+    setSelectedTrainerId(selectedId !== 0 ? selectedId : null);
+  };
+
   return (
     <>
       <h1>담당 트레이너</h1>
 
-      <select>
-        <option>선택</option>
+      <select onChange={handleTrainer}>
+        <option value={0}>선택</option>
         {trainers.map((trainer: any) => (
           <option key={trainer.id} value={trainer.id}>
             {trainer.name}
           </option>
         ))}
       </select>
+      <hr />
+      <CustomerByTrainer />
     </>
   );
 };
